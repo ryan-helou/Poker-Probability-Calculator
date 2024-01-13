1590637ec2af550ecc0d1855054c189c966b26b6
RyanHelou <ryan.helou@gmail.com>
2024-01-13 18:06:03 -0500

    test commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args)
     {
-
-        Round.simulateMultipleRounds(14,"Spades",14,"Clubs", 4,50000);
+//test
+        Round.simulateMultipleRounds(12,"Spades",14,"Clubs", 4,50000);
 
     }
 
