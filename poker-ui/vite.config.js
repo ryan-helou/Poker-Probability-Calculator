6118cde1a1fda37b97961ea4cf9a0838e46fbfdc
Ryan Helou <ryan.helou@gmail.com>
2025-10-16 12:07:27 -0400

    bunch of stuff

--- poker-ui/vite.config.js
+++ poker-ui/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Poker-Probability-Calculator/', // IMPORTANT
+  build: { outDir: 'docs' },     
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
